bdce8b753534e19bb7db71c2bd152e90d4b59e9e
AetherFabian <fabian_galarza@hotmail.com>
2022-03-25 21:49:31 -0700

    add the param id in the delete endpoint

--- app/main.py
+++ app/main.py
@@ -87,7 +87,7 @@ class Student(Resource):
         del student['_id']
         return jsonify(student)
 
-    def delete(self):
+    def delete(self, id):
         student = self.abort_if_not_exists(id)
         database.db.students.delete_one({'id': id})
         del student['_id']
