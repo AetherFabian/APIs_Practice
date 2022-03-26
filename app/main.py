41dc897d37ca08b14413e506ca11a7e91f164a10
AetherFabian <fabian_galarza@hotmail.com>
2022-03-25 21:58:42 -0700

    patch arguments, not required

--- app/main.py
+++ app/main.py
@@ -19,10 +19,10 @@ post_students_args.add_argument("career", type=str, required = False)
 
 patch_students_args = reqparse.RequestParser()
 
-patch_students_args.add_argument("id", type=int, help="ERROR id value needs to be an integer", required = True)
-patch_students_args.add_argument("first_name", type=str, help="ERROR first_name is required", required = True)
-patch_students_args.add_argument("last_name", type=str, help="ERROR last_name is required", required = True)
-patch_students_args.add_argument("image", type=str, help="ERROR you need to add the image url", required = True)
+patch_students_args.add_argument("id", type=int, help="ERROR id value needs to be an integer", required = False)
+patch_students_args.add_argument("first_name", type=str, help="ERROR first_name is required", required = False)
+patch_students_args.add_argument("last_name", type=str, help="ERROR last_name is required", required = False)
+patch_students_args.add_argument("image", type=str, help="ERROR you need to add the image url", required = False)
 patch_students_args.add_argument("group", type=str, required = False)
 patch_students_args.add_argument("career", type=str, required = False)
 
